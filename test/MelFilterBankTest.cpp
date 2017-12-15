f3d49425e71969f79204dcfea412a33010b594e4
Cinek2 <marcinb280@op.pl>
2017-12-15 01:10:19 +0100

    Reconfigured CMakeLists to find boost libraries

--- test/MelFilterBankTest.cpp
+++ test/MelFilterBankTest.cpp
@@ -8,7 +8,7 @@
 #define BOOST_TEST_MODULE MelFilterBankTest
 #include <iostream>
 #include "../src/MelFilterBank.h"
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 
 BOOST_AUTO_TEST_SUITE(MelFilterBankTest)
@@ -16,8 +16,6 @@ BOOST_AUTO_TEST_SUITE(MelFilterBankTest)
 BOOST_AUTO_TEST_CASE( invalid )
 {
     BOOST_CHECK(false);
-    BOOST_CHECK(false);
-    std::cout << "bellelele";
     BOOST_CHECK_EQUAL(2,1);
 }
 BOOST_AUTO_TEST_SUITE_END()
