0de95807701cc8245ee1ea603d730cc93165c5a2
Cinek28 <marcinb280@op.pl>
2017-12-16 13:26:54 +0100

    Fixed boost tests linking and unreferenced libraries to test

--- test/MelFilterBankTest.cpp
+++ test/MelFilterBankTest.cpp
@@ -4,19 +4,17 @@
 #ifndef WIN32
 #define BOOST_TEST_DYN_LINK
 #endif
-
 #define BOOST_TEST_MODULE MelFilterBankTest
-
+#include <boost/test/unit_test.hpp>
 #include <iostream>
 #include <MelFilterBank.h>
-#include <boost/test/unit_test.hpp>
-
 
 BOOST_AUTO_TEST_SUITE(MelFilterBankTest)
 
 BOOST_AUTO_TEST_CASE( CheckConversion )
-        {		
-                //BOOST_CHECK_EQUAL(MelFilterBank::melToHertz(2595.0), 0.0);
+        {
+                BOOST_CHECK_EQUAL(MelFilterBank::hertzToMel(0.0), 0.);
+                BOOST_CHECK_EQUAL(MelFilterBank::melToHertz(0.0), 0.0);
 				BOOST_CHECK_EQUAL(0, 0);
         }
 BOOST_AUTO_TEST_SUITE_END()
