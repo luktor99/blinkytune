52f735bbf74f9fdef563c9ba2520a2e79d6d62cb
Łukasz K <luktor99@gmail.com>
2018-01-14 17:23:48 +0100

    Added EffectsController documentation

--- src/EffectsController.h
+++ src/EffectsController.h
@@ -17,26 +17,50 @@
 /**
  * Represents a facade for the whole audio processing and effect generation system. This is the main component of the
  * program that controls the whole life cycle of all processing threads and keeps the connection to the LED strip. This
- * class uses the singleton and facade design pattern.
+ * class uses the singleton and facade design patterns.
  */
 class EffectsController {
 public:
     /**
     * Retrieve a instance of singleton class EffectsController.
-    * @return EffectsController singleton object.
+    * @return EffectsController instance.
     */
     static EffectsController &getInstance();
 
+    /**
+     * Establish connection with the LED strip.
+     * @param ip The IP of the LED strip.
+     * @param length The number of LEDs in the strip.
+     */
     void connect(const std::string &ip, int length);
 
+    /**
+     * Retrieve the object of currently running effect.
+     * @return A pointer to the currently active Effect object.
+     */
     Effect *getEffect() const;
 
+    /**
+     * Set the input audio device.
+     * @param device A reference the the desired AudioDevice object.
+     */
     void setAudioDevice(const AudioDevice &device);
 
+    /**
+     * Set the desired update rate of the LED strip (in frames per seconds).
+     * @param fps The number of frames per seconds.
+     */
     void setFPS(const float &fps);
 
+    /**
+     * Set the active light effect.
+     * @param effectName The name of the effect.
+     */
     void setEffect(const std::string &effectName);
 
+    /**
+     * Stop the effects controler. This function stops all processing threads and disconnects from the LED strip.
+     */
     void stop();
 
 private:
@@ -54,34 +78,55 @@ private:
 
     EffectsController &operator=(const EffectsController &) = delete;
 
+    /**
+     * Start the processing threads required to render sound effects.
+     */
     void startSoundPipeline();
 
+    /**
+     * Start the processing threads required to render no sound effects.
+     */
     void startNoSoundPipeline();
 
+    /**
+    * Stop the processing threads required to render sound effects.
+    */
     void stopSoundPipeline();
 
+    /**
+    * Stop the processing threads required to render no sound effects.
+    */
     void stopNoSoundPipeline();
 
+    /**
+     * Set the active effect.
+     * @param effect A pointer to the Effect object.
+     */
     void setActiveEffect(Effect *effect);
 
+    /**
+     * The possible effect processing modes.
+     */
     enum activePipelineType {
-        PIPELINE_NONE = 0, PIPELINE_SOUND, PIPELINE_NOSOUND
+        PIPELINE_NONE = 0,  /*!< No processing threads are active. */
+        PIPELINE_SOUND,     /*!< Processing threads required to render sound effects are active. */
+        PIPELINE_NOSOUND    /*!< Processing threads required to render no sound effects are active. */
     };
-    activePipelineType activePipeline;
 
-    std::unique_ptr<LEDStrip> ledStrip_;
-    std::unique_ptr<EffectsRenderer> effectsRenderer_;
-    std::unique_ptr<Effect> effect_;
-    std::unique_ptr<SamplesCollector> samplesCollector_;
-    std::unique_ptr<SpectrumGenerator> spectrumGenerator_;
-    std::unique_ptr<SpectrumAnalyzer> spectrumAnalyzer_;
-    std::unique_ptr<TickGenerator> tickGenerator_;
-    AudioDevice audioDevice_;
-    std::unique_ptr<AudioInputStream> audioStream_;
-    FIFOQueue<StereoSamplesBuffer> samplesFIFO_;
-    FIFOQueue<StereoSpectrumBuffer> spectrumFIFO_;
-    FIFOQueue<StereoAnalysisBuffer> analysisFIFO_;
-    float refreshRate_;
+    activePipelineType activePipeline; /*!< The current effect processing mode. */
+    std::unique_ptr<LEDStrip> ledStrip_; /*!< A unique_ptr to the active LEDStrip object. */
+    std::unique_ptr<EffectsRenderer> effectsRenderer_; /*!< A unique_ptr to the EffectsRenderer thread. */
+    std::unique_ptr<Effect> effect_; /*!< A unique_ptr to the active Effect object. */
+    std::unique_ptr<SamplesCollector> samplesCollector_; /*!< A unique_ptr to the SamplesCollector thread. */
+    std::unique_ptr<SpectrumGenerator> spectrumGenerator_; /*!< A unique_ptr to the SpectrumGenerator thread. */
+    std::unique_ptr<SpectrumAnalyzer> spectrumAnalyzer_; /*!< A unique_ptr to the SpectrumAnalyzer thread. */
+    std::unique_ptr<TickGenerator> tickGenerator_; /*!< A unique_ptr to the TickGenerator thread. */
+    AudioDevice audioDevice_; /*!< The currently selected AudioDevice object. */
+    std::unique_ptr<AudioInputStream> audioStream_; /*!< A unique_ptr to the active AudioInputStream object. */
+    FIFOQueue<StereoSamplesBuffer> samplesFIFO_; /*!< A FIFO queue used to pass samples data between threads. */
+    FIFOQueue<StereoSpectrumBuffer> spectrumFIFO_; /*!< A FIFO queue used to pass spectrum data between threads. */
+    FIFOQueue<StereoAnalysisBuffer> analysisFIFO_; /*!< A FIFO queue used to pass analysis data between threads. */
+    float refreshRate_; /*!< The currently selected update rate of the LED strip */
 };
 
 
