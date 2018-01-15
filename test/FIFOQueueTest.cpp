41a5ea9b53062de90d8285506d1b822e55e4cf3b
Łukasz K <luktor99@gmail.com>
2018-01-15 23:29:02 +0100

    Fixed automated tests

---
+++ test/FIFOQueueTest.cpp
@@ -0,0 +1,37 @@
+//
+// Created by luktor99 on 15.01.18.
+//
+#include <boost/test/unit_test.hpp>
+
+#include <FIFOQueue.h>
+
+BOOST_AUTO_TEST_SUITE(FIFOQueueTest)
+
+    BOOST_AUTO_TEST_CASE(PushElementToAnEmptyQueueAndPopIt) {
+        FIFOQueue<int> queue(5);
+        queue.push(new int(123));
+        BOOST_CHECK_EQUAL(*queue.pop(), 123);
+    }
+
+    BOOST_AUTO_TEST_CASE(PushMultipleElementsToAnEmptyQueueAndPopThem) {
+        FIFOQueue<int> queue(3);
+        queue.push(new int(1));
+        queue.push(new int(2));
+        queue.push(new int(3));
+        BOOST_CHECK_EQUAL(*queue.pop(), 1);
+        BOOST_CHECK_EQUAL(*queue.pop(), 2);
+        BOOST_CHECK_EQUAL(*queue.pop(), 3);
+    }
+
+    BOOST_AUTO_TEST_CASE(PushElementToAFullQueueAndCheckIfTheLastItemHasBeenOverwritten) {
+        FIFOQueue<int> queue(3);
+        queue.push(new int(1));
+        queue.push(new int(2));
+        queue.push(new int(3));
+        queue.push(new int(4));
+        BOOST_CHECK_EQUAL(*queue.pop(), 2);
+        BOOST_CHECK_EQUAL(*queue.pop(), 3);
+        BOOST_CHECK_EQUAL(*queue.pop(), 4);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ test/TestsMain.cpp
@@ -0,0 +1,10 @@
+//
+// Created by luktor99 on 15.01.18.
+//
+#ifndef WIN32
+#define BOOST_TEST_DYN_LINK
+#endif
+
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE blinky_tune_tests
+#include <boost/test/unit_test.hpp>
