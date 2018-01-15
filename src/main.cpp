77bb27c0a5ffcb383285f7c0a57a9b467edb09e3
Łukasz K <luktor99@gmail.com>
2018-01-15 21:50:27 +0100

    Removed unused imports from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include <QApplication>
 #include <QSplashScreen>
 #include <qthread.h>
@@ -8,39 +6,32 @@
 
 #include "AudioInterface.h"
 
-#include "FIFOQueue.h"
-#include "StereoSamplesBuffer.h"
-#include "SamplesCollector.h"
-#include "EffectsRenderer.h"
-#include "EffectsController.h"
-#include "EffectsFactory.h"
-
 
 int main(int argc, char **argv) {
     // Supress warnings:
     (void) argc;
     (void) argv;
 
-	// Start application
-	QApplication app(argc, argv);
+    // Start application
+    QApplication app(argc, argv);
 
-	// Show splash screen
-	QPixmap pixmap("../../src/logo.png");
-	QSplashScreen splash(pixmap);
-	splash.show();
-	app.processEvents();
+    // Show splash screen
+    QPixmap pixmap("../../src/logo.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
+    app.processEvents();
 
-	// Initialize AudioInterface:
-	AudioInterface::getInstance().initialize();
+    // Initialize AudioInterface:
+    AudioInterface::getInstance().initialize();
 
-	MainWindow mainWin;
-	mainWin.setWindowTitle("BlinkyTune");
-	mainWin.setWindowIcon(pixmap);
-	mainWin.setupUi();
-	QThread::sleep(2); // the splash is shown for 2 seconds
-	mainWin.show();
-	splash.finish(&mainWin);
+    MainWindow mainWin;
+    mainWin.setWindowTitle("BlinkyTune");
+    mainWin.setWindowIcon(pixmap);
+    mainWin.setupUi();
+    QThread::sleep(2); // the splash is shown for 2 seconds
+    mainWin.show();
+    splash.finish(&mainWin);
 
-	return app.exec();
+    return app.exec();
 
 }
