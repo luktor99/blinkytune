6598f4db76c45243a8edfdcc6c9db2f604459af2
Cinek2 <marcinb280@op.pl>
2018-01-18 23:53:35 +0100

    Added EffectsFactory tests

--- test/EffectsFactoryTest.cpp
+++ test/EffectsFactoryTest.cpp
@@ -4,11 +4,33 @@
 #include <boost/test/unit_test.hpp>
 
 #include <EffectsFactory.h>
+#include <typeinfo>
+#include <iostream>
 
 BOOST_AUTO_TEST_SUITE(EffectsFactoryTest)
 
-   BOOST_AUTO_TEST_CASE(Check) {
-	BOOST_CHECK_EQUAL(0,0);
+   BOOST_AUTO_TEST_CASE(CheckIfGetsSingletonInstance) {
+	const EffectsFactory& interface = EffectsFactory::getInstance();
+	BOOST_CHECK_EQUAL(&EffectsFactory::getInstance(), &interface);
     }
+
+BOOST_AUTO_TEST_CASE(CheckIfProducesRequiredObject) {
+	EffectsFactory& interface = EffectsFactory::getInstance();
+	BOOST_CHECK_EQUAL(typeid(*interface.createEffect("Still Color")).name(), "class StillColor");
+	BOOST_CHECK_EQUAL(typeid(*interface.createEffect("Color Beat")).name(), "class ColorBeat");
+	BOOST_CHECK_EQUAL(typeid(*interface.createEffect("Color Spectrum")).name(), "class ColorSpectrum");
+	BOOST_CHECK_EQUAL(typeid(*interface.createEffect("Rainbow")).name(), "class Rainbow");
+	BOOST_CHECK_EQUAL(interface.createEffect("Unknown"), nullptr);
+}
+
+BOOST_AUTO_TEST_CASE(CheckProducedObjectsNames) {
+	EffectsFactory& interface = EffectsFactory::getInstance();
+	const std::vector<std::string> noSoundEffects = interface.getNoSoundEffects();
+	const std::vector<std::string> soundEffects = interface.getSoundEffects();
+	BOOST_CHECK(std::find(noSoundEffects.begin(), noSoundEffects.end(), "Still Color") != noSoundEffects.end());
+	BOOST_CHECK(std::find(noSoundEffects.begin(), noSoundEffects.end(), "Rainbow") != noSoundEffects.end());
+	BOOST_CHECK(std::find(soundEffects.begin(), soundEffects.end(), "Color Beat") != soundEffects.end());
+	BOOST_CHECK(std::find(soundEffects.begin(), soundEffects.end(), "Color Spectrum") != soundEffects.end());
+}
     
 BOOST_AUTO_TEST_SUITE_END()
