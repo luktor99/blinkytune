dff02e7687cf236344b8a51be5d1ef5e4389b33f
Cinek2 <marcinb280@op.pl>
2018-01-19 04:09:28 +0100

    Hopefully last fix of EffectsFactory

--- test/EffectsFactoryTest.cpp
+++ test/EffectsFactoryTest.cpp
@@ -16,21 +16,25 @@ BOOST_AUTO_TEST_SUITE(EffectsFactoryTest)
 
 BOOST_AUTO_TEST_CASE(CheckIfProducesRequiredObject) {
 	EffectsFactory& interface = EffectsFactory::getInstance();
-	BOOST_CHECK_EQUAL(typeid(*interface.createEffect("Still Color")).name(), "class StillColor");
-	BOOST_CHECK_EQUAL(typeid(*interface.createEffect("Color Beat")).name(), "class ColorBeat");
-	BOOST_CHECK_EQUAL(typeid(*interface.createEffect("Color Spectrum")).name(), "class ColorSpectrum");
-	BOOST_CHECK_EQUAL(typeid(*interface.createEffect("Rainbow")).name(), "class Rainbow");
-	BOOST_CHECK_EQUAL(interface.createEffect("Unknown"), nullptr);
+	std::string name = "class StillColor";
+	BOOST_CHECK(typeid(*interface.createEffect("Still Color")).name() == name);
+	name = "class ColorBeat";
+	BOOST_CHECK(typeid(*interface.createEffect("Color Beat")).name() == name);
+	name = "class ColorSpectrum";
+	BOOST_CHECK(typeid(*interface.createEffect("Color Spectrum")).name() == name);
+	name = "class Rainbow";
+	BOOST_CHECK(typeid(*interface.createEffect("Rainbow")).name() == name);
+	BOOST_CHECK(interface.createEffect("Unknown") == nullptr);
 }
 
-/*BOOST_AUTO_TEST_CASE(CheckProducedObjectsNames) {
+BOOST_AUTO_TEST_CASE(CheckProducedObjectsNames) {
 	EffectsFactory& interface = EffectsFactory::getInstance();
 	const std::vector<std::string> noSoundEffects = interface.getNoSoundEffects();
 	const std::vector<std::string> soundEffects = interface.getSoundEffects();
-	BOOST_REQUIRE(std::find(noSoundEffects.begin(), noSoundEffects.end(), "Still Color") != noSoundEffects.end());
-	BOOST_REQUIRE(std::find(noSoundEffects.begin(), noSoundEffects.end(), "Rainbow") != noSoundEffects.end());
-	BOOST_REQUIRE(std::find(soundEffects.begin(), soundEffects.end(), "Color Beat") != soundEffects.end());
-	BOOST_REQUIRE(std::find(soundEffects.begin(), soundEffects.end(), "Color Spectrum") != soundEffects.end());
-}*/
+	BOOST_CHECK(std::find(noSoundEffects.begin(), noSoundEffects.end(), "Still Color") != noSoundEffects.end());
+	BOOST_CHECK(std::find(noSoundEffects.begin(), noSoundEffects.end(), "Rainbow") != noSoundEffects.end());
+	BOOST_CHECK(std::find(soundEffects.begin(), soundEffects.end(), "Color Beat") != soundEffects.end());
+	BOOST_CHECK(std::find(soundEffects.begin(), soundEffects.end(), "Color Spectrum") != soundEffects.end());
+}
     
 BOOST_AUTO_TEST_SUITE_END()
